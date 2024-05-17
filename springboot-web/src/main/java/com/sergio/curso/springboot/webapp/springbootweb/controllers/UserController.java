5231628ee37f76371957c16ff938acebe30b905d
fernandoguzvar <80289909+fernandoguzvar@users.noreply.github.com>
2024-05-17 12:25:31 -0500

    Update controller
    Básicamente lo usamos para mostrar por consola o mejor aun en formato json a traves de la url que especificamos

--- springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -4,15 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sergio.curso.springboot.webapp.springbootweb.models.User;
+
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details (Model model) {
+    public String details(Model model) {
+        // Haciendo uso de nuestra clase User.java
+        User user = new User("Sergio Andres", "Perdomo Ortiz");
         // Pasandole datos a la vista
         model.addAttribute("title", "Hola Mundo SpringBoot");
-        model.addAttribute("name", "Sergio Andres");
-        model.addAttribute("lastName", "Perdomo Ortiz");
+        model.addAttribute("user", user);
         return "details";
     }
 
