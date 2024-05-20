9318d3d8ad012ae392c7ecb506d74f216a3264ec
fernandoguzvar <80289909+fernandoguzvar@users.noreply.github.com>
2024-05-19 23:55:12 -0500

    Uso DTO
    Aprendiendo a usar un DTO

--- springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/controllers/UserResController.java
+++ springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/controllers/UserResController.java
@@ -1,29 +1,56 @@
 package com.sergio.curso.springboot.webapp.springbootweb.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sergio.curso.springboot.webapp.springbootweb.models.User;
-
+import com.sergio.curso.springboot.webapp.springbootweb.models.dto.UserDto;
 
 // A continuación vas a ver una API REST
 @RestController
 @RequestMapping("/api")
 public class UserResController {
 
-    // Haciendo uso de nuestra clase User.java
-    User user = new User("Sergio Andres", "Perdomo Ortiz");
+    @GetMapping("/details")
+    public UserDto details() {
+        // Haciendo uso de nuestra clase User.java
+        User user = new User("Sergio Andres", "Perdomo Ortiz");
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTittle("Hello World");
 
-    @GetMapping("/detailsTwo")
-    public Map<String, Object> details () {
+        return userDto;
+
+    }
+
+    // Método de lista
+    @GetMapping("/list")
+    public List <User> list() {
+        User user = new User("Ana", "Francheska");
+        User user1 = new User("Tum", "Reng");
+        User user2 = new User("Renata", "Nuñez");
+
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user1);
+        users.add(user2);
+
+        return users;
+    }
+
+
+
+    @GetMapping("/details-map")
+    public Map<String, Object> detailsMap() {
+        User user = new User("Sergio", "Fede");
         Map<String, Object> body = new HashMap<>();
         body.put("title", "Hola Mundo SpringBoot");
-        body.put("user", user);
         body.put("user", user);
         return body;
     }
