b663f9e201bd6eb30af539abb227bf9a8d89e9ce
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-06-09 22:00:31 -0500

    Pasando por medio de la ruta los valores que se nos mostraran en formato json

--- springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/controllers/RequestParamsController.java
+++ springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/controllers/RequestParamsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sergio.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
 import com.sergio.curso.springboot.webapp.springbootweb.models.dto.ParamMixDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/params")
 public class RequestParamsController {
@@ -27,5 +29,19 @@ public class RequestParamsController {
         params.setCode(code);
         return params;
     }
+
+    @GetMapping("/request")
+    public ParamMixDto request (HttpServletRequest request){
+        Integer code = 0;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        }catch (NumberFormatException e){}
+
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
+        return params;
+    }
+    
 
 }
