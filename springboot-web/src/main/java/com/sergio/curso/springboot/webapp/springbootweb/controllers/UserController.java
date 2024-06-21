cdd0ec771d47efe9628ffd17e79501fde8af2253
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-06-21 09:58:19 -0500

    Puse como comentario esa línea de código en especifico.

--- springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.sergio.curso.springboot.webapp.springbootweb.models.User;
 // import org.springframework.web.bind.annotation.RequestParam;
