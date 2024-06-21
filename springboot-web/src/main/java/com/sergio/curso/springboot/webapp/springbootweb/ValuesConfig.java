5177a22c20bfd96dceed2e465ac765d5105d0416
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-06-21 11:56:09 -0500

    Enlazando el fichero que se encuentra con las propiedades en esta nueva clase.

---
+++ springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/ValuesConfig.java
@@ -0,0 +1,13 @@
+package com.sergio.curso.springboot.webapp.springbootweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:values.properties"), // @PropertySources podemos configurar más de un archivo, @PropertySource solo sirve para uno en especifico.
+        //@PropertySource("classpath:values.properties2")
+})
+public class ValuesConfig {
+}
