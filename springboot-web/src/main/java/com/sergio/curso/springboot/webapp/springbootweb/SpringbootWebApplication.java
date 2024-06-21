f45c5d3c496436f6f818228e262582575a9802dc
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-06-21 11:54:48 -0500

    Añadiendole el fichero en donde tenemos las propiedades que creamos de manera personalizada

--- springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/SpringbootWebApplication.java
+++ springboot-web/src/main/java/com/sergio/curso/springboot/webapp/springbootweb/SpringbootWebApplication.java
@@ -2,10 +2,10 @@ package com.sergio.curso.springboot.webapp.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication
-@PropertySource("classpath:values.properties")
+
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
