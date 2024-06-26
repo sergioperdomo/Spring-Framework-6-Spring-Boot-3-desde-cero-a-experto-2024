aa28d9bc8b967c6f3caa1a5fb3dc4735a352e72f
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-06-26 16:39:11 -0500

    Changes

---
+++ springbootdi/src/main/java/com/sergio/springboot/di/app/springbootdi/SpringbootdiApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.springboot.di.app.springbootdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdiApplication.class, args);
+	}
+
+}
